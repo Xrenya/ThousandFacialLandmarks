f0f6942075cc4640cad4d571e260af4297b40dd5
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-05-01 20:44:01 +0900

    Add files via upload

---
+++ train.py
@@ -0,0 +1,17 @@
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(config_path="configs/", config_name="train.yaml")
+def main(config: DictConfig):
+
+    # Imports can be nested inside @hydra.main to optimize tab completion
+    # https://github.com/facebookresearch/hydra/issues/934
+    from detector.engine.trainer import train
+
+    # Train model
+    return train(config)
+
+
+if __name__ == "__main__":
+    main()
